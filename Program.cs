24e3cef278642e598acb0d5fc4ac6e9db8589516
Elvio Flauzino <cristiano.flauzino>
2025-10-30 12:17:07 -0300

    Desafio OPP concluido II

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1231313", modelo: "46444", imei: "1111111", memoria: 12);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhastApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "9999999", modelo: "14GS", imei: "56565656", memoria: 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
